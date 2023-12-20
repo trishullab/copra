6027bd7972827f00710e8b348a2d99feaf3d04d9
Jimmy Xin <jxin31415@gmail.com>
2023-12-20 19:09:27 +0000

    initial commit - identifying places to add code

--- src/tools/proof_exec_callback.py
+++ src/tools/proof_exec_callback.py
@@ -14,6 +14,7 @@ from src.tools.coq_executor import CoqExecutor
 from src.tools.lean_cmd_executor import Lean3Executor
 from src.tools.dynamic_coq_proof_exec import DynamicProofExecutor as DynamicCoqProofExecutor
 from src.tools.dynamic_lean_proof_exec import DynamicProofExecutor as DynamicLeanProofExecutor
+from src.tools.dynamic_isabelle_proof_exec import DynamicProofExecutor as DynamicIsabelleProofExecutor
 
 class ProofExecutorCallback(object):
     def __init__(self,
@@ -41,7 +42,7 @@ class ProofExecutorCallback(object):
         self.always_use_retrieval = always_use_retrieval
         pass
 
-    def get_proof_executor(self) -> typing.Union[DynamicCoqProofExecutor, DynamicLeanProofExecutor]:
+    def get_proof_executor(self) -> typing.Union[DynamicCoqProofExecutor, DynamicLeanProofExecutor, DynamicIsabelleProofExecutor]:
         if self.language == ProofAction.Language.COQ:
             search_exec = CoqExecutor(self.project_folder, self.file_path, use_hammer=self.use_hammer, timeout_in_sec=self.timeout_in_secs, suppress_error_log=self.suppress_error_log, use_human_readable_proof_context=self.use_human_readable_proof_context)
             coq_context_helper = CoqContextHelper(search_exec, self.search_depth, logger=self.logger)
@@ -49,6 +50,11 @@ class ProofExecutorCallback(object):
         elif self.language == ProofAction.Language.LEAN:
             search_exec = Lean3Executor(self.project_folder, self.prefix, self.file_path, use_hammer=self.use_hammer, timeout_in_sec=self.timeout_in_secs, suppress_error_log=self.suppress_error_log, use_human_readable_proof_context=self.use_human_readable_proof_context, enable_search=self.always_use_retrieval)
             lean_context_helper = Lean3ContextHelper(search_exec, self.search_depth, logger=self.logger)
-            return DynamicLeanProofExecutor(lean_context_helper, self.project_folder, self.file_path, context_type=DynamicCoqProofExecutor.ContextType.NoContext, use_hammer=self.use_hammer, timeout_in_seconds=self.timeout_in_secs, suppress_error_log=self.suppress_error_log, use_human_readable_proof_context=self.use_human_readable_proof_context)
+            return DynamicLeanProofExecutor(lean_context_helper, self.project_folder, self.file_path, context_type=DynamicLeanProofExecutor.ContextType.NoContext, use_hammer=self.use_hammer, timeout_in_seconds=self.timeout_in_secs, suppress_error_log=self.suppress_error_log, use_human_readable_proof_context=self.use_human_readable_proof_context)
+        elif self.language == ProofAction.Language.ISABELLE:
+            # TODO: jimmy - currently uses Coq backend
+            search_exec = CoqExecutor(self.project_folder, self.file_path, use_hammer=self.use_hammer, timeout_in_sec=self.timeout_in_secs, suppress_error_log=self.suppress_error_log, use_human_readable_proof_context=self.use_human_readable_proof_context)
+            isabelle_context_helper = CoqContextHelper(search_exec, self.search_depth, logger=self.logger)
+            return DynamicIsabelleProofExecutor(isabelle_context_helper, self.project_folder, self.file_path, context_type=DynamicIsabelleProofExecutor.ContextType.BestContext, use_hammer=self.use_hammer, timeout_in_seconds=self.timeout_in_secs, suppress_error_log=self.suppress_error_log, use_human_readable_proof_context=self.use_human_readable_proof_context)
         else:
             raise Exception(f"Unknown context type: {self.context_type}")
