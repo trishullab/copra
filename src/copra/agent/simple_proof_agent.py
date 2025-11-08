431cb685885d646882c850e413d75345f006e853
Amitayush Thakur <amitayush@utexas.edu>
2025-11-08 00:35:26 +0000

    Removed `have` complicated logic for the ProofAgent.

--- src/copra/agent/simple_proof_agent.py
+++ src/copra/agent/simple_proof_agent.py
@@ -61,46 +61,20 @@ class ProofAgent(Agent):
             policy_info_message: typing.Callable[[int, typing.Dict[str, typing.Any]], str],
             render: bool):
         env.reset()
-        lean_hack = HandleHaveTactic()
         done = False
         steps = 0
         total_reward = 0
         next_state = env.state
         additional_info = self._policy.get_efficiency_info()
-        action_fixed = True
         while not done and not stop_policy(steps, additional_info):
             self.logger.info(policy_info_message(steps, additional_info))
             self.logger.info("Asking policy for next action")
             action = self._policy(next_state)
             assert isinstance(action, ProofAction)
             self.logger.info(f"Got Action: {action}")
-            action = lean_hack.parse_have_tactic_action(action)
-            self.logger.info(f"Parsed Action: {action}")
             if action.action_type != ProofAction.ActionType.EXIT:
-                action_fixed, indented_action = lean_hack.fix_action(action, self.logger)
-                assert action_fixed, f"Action {action} is not fixed"
-                modified_action = copy.deepcopy(action)
-                has_more_than_one_tactic = len(action.kwargs.get('tactics', [])) > 1
-                is_have_tactic = lean_hack.is_single_line_have_tactic(action)
-                if modified_action.action_type == ProofAction.ActionType.RUN_TACTIC:
-                    modified_action.kwargs['tactics'] = [indented_action.kwargs['tactics'][0]]
-                    if len(action.kwargs.get('tactics', [])) > 1:
-                        self._policy.reset_last_action(modified_action)
-                self.logger.info(f"Action to run: {modified_action}")
-                state, modified_action, next_state, reward, done, info = env.step(modified_action)
-                # **IMPORTANT NOTE**: Here we update the action because sometimes the proof env can optimize the action
-                # and return a different action which kind of aligns with the action taken by the
-                # policy but only more efficient. This is slightly different traditional RL setting
-                ignore_single_line_have = (not has_more_than_one_tactic) and is_have_tactic
-                lean_hack.scope_state(state, modified_action, next_state, info, self.logger, ignore_single_line_have)
-                if lean_hack.is_within_have_tactic():
-                    assert isinstance(next_state, ProofState)
-                    if next_state.training_data_format.goal_description is None:
-                        next_state.training_data_format.goal_description = ""
-                    last_have_tactic = lean_hack.get_last_have_tactic()
-                    assert last_have_tactic is not None, f"Last have tactic is None, {lean_hack._have_tactics}"
-                    next_state.training_data_format.goal_description += \
-                    f"IMPORTANT NOTE: Working on the sub-goal with have tactic: \n{last_have_tactic}."
+                self.logger.info(f"Action to run: {action}")
+                state, modified_action, next_state, reward, done, info = env.step(action)
                 if render:
                     self.logger.info("**"*20)
                     env.render()
@@ -109,19 +83,8 @@ class ProofAgent(Agent):
                     # Don't update policy for backtracking actions, this will create a 
                     # a very nasty loop in the policy.
                     self.logger.info("Updating policy")
-                    self._policy.update(state, action, next_state, reward, done, info)
+                    self._policy.update(state, modified_action, next_state, reward, done, info)
                     self.logger.info("Policy updated")
-                if action.action_type == ProofAction.ActionType.RUN_TACTIC and \
-                    info.progress != ProgressState.FAILED and \
-                    len(action.kwargs.get('tactics', [])) > 1:
-                    remaining_tactics = action.kwargs.get('tactics', [])[1:]
-                    remaining_tactics.reverse()
-                    for tactic in remaining_tactics:
-                        modified_action : ProofAction = copy.deepcopy(action)
-                        modified_action.kwargs['tactics'] = [tactic]
-                        modified_action.original_message["content"] = f"[RUN TACTIC]\n{tactic}\n[END]"
-                        self.logger.info(f"Adding delayed action: {modified_action}")
-                        self._policy.add_delayed(modified_action)
                 steps += 1
                 total_reward += reward
                 additional_info = self._policy.get_efficiency_info()
